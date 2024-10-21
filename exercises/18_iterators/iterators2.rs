6eecec56be44bef1d3eb78cb71a5ab2b1c7fd372
soundofautumn <qjm1412@163.com>
2024-10-21 19:41:52 +0800

    feat: exercise 18

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_uppercase().to_string() + chars.as_str(),
     }
 }
 
@@ -16,6 +16,11 @@ fn capitalize_first(input: &str) -> String {
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
     // ???
+    let mut output = Vec::new();
+    for &word in words {
+        output.push(capitalize_first(word));
+    }
+    output
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
@@ -23,6 +28,7 @@ fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
     // ???
+    String::from_iter(capitalize_words_vector(words))
 }
 
 fn main() {
